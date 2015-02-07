8fe749b9b815c3c3ba7981d63a941fa467eef63a
Ardra Hren <ardra_hren@brown.edu>
2015-02-07 16:45:44 -0500

    updated model

--- shatter/models.py
+++ shatter/models.py
@@ -3,22 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Comment(models.Model):
-    companyId = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     comment = models.CharField(max_length=500)
-    rating = models.CharField(max_length=2)
+    rating = models.DecimalField(max_digits=None, decimal_places=None)
     tag = models.CharField(max_length=15)
+    company = models.ForeignKey('Company')
     def __str__(self):
         return self.comment
 
 class Company(models.Model):
     name = models.CharField(max_length=50) 
-    uniqueId = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
     employees = models.FloatField()
     website = models.URLField()
-    diversity = models.DecimalField(max_digits=2)
-    comments = models.ManyToManyField(Comment)
-
+    statistic = models.OnetoOneField('Statistic')
+    diversity = models.DecimalField(max_digits=None, decimal_places=None)
+    
     def __str__(self):
         return self.name
+
+class Statistic(models.Model):
+	diversity = models.DecimalField(max_length=4)
+	inclusivity = models.DecimalField(max_length=4)
+
+
+
 
