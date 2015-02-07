560ef8d5528e2f609c9566f9535f05189099c007
Aisha Ferrazares <aisha.ferrazares@gmail.com>
2015-02-07 16:11:15 -0500

    added comment/company models

--- shatter/models.py
+++ shatter/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    companyId = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    comment = models.CharField(max_length=500)
+    rating = models.CharField(max_length=2)
+    tag = models.CharField(max_length=15)
+    def __str__(self):
+        return self.comment
+
+class Company(models.Model):
+    name = models.CharField(max_length=50) 
+    uniqueId = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+    employees = models.FloatField()
+    website = models.URLField()
+    diversity = models.DecimalField(max_digits=2)
+    comments = models.ManyToManyField(Comment)
+
+    def __str__(self):
+        return self.name
+
