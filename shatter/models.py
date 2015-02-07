ecb628cf4e66cd0f38902b20982e6e350d39d499
Ardra Hren <ardra_hren@brown.edu>
2015-02-07 18:18:57 -0500

    resolved

--- shatter/models.py
+++ shatter/models.py
@@ -14,8 +14,20 @@ class Comment(models.Model):
 class Company(models.Model):
     name = models.CharField(max_length=50) 
     website = models.URLField()
-    #statistic = models.OnetoOneField('Statistic')
-    #diversity = models.DecimalField(max_digits=None, decimal_places=None)
+
+    percent_women = models.FloatField()
+    # statistic = models.OneToOneField('Statistic', null=True)#default= Statistic(diversity = 0.00, inclusivity = 0.00))
+    # statistics = ForeignKey('')
+	
+    overall_rating = models.FloatField()
+    diversity = models.FloatField()
+    inclusivity = models.FloatField()
+
+    # overall_diversity = through_fields('Tag_Type', models.IntegerField())
+    # diversity = through_fields('Tag_Type', models.IntegerField())
+    # inclusivity = through_fields('Tag_Type', models.IntegerField())
+    
+
     def __str__(self):
         return self.name
 
